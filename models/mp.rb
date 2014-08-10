57c19b1e63ae8f906ce32d76d33b7057c95d0e87
Caleb Tutty <caleb@prettymint.co.nz>
2014-08-10 21:02:35 +1200

    Tweaking scraper

--- models/mp.rb
+++ models/mp.rb
@@ -1,4 +1,28 @@
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
 class MP < Sequel::Model
+  def as_json
+    {
+      id: id,
+      first_name: first_name,
+      last_name: last_name,
+      party: party,
+      list_mp: list_mp,
+      details_url: details_url,
+      image_url: image_url
+    }
+  end
+
+  private
+
+  def image_from_src(src)
+    BASE_URL + src
+  end
+
+  def parse_electorate(electorate_name)
+    if electorate_name == 'List'
+      electorate = nil
+      list_mp = true
+    end
+  end
 end
--- scraper.rb
+++ scraper.rb
@@ -7,8 +7,18 @@ Bundler.require(:default, :scraper)
 
 require 'models/mp'
 
-MP_LIST_URL = 'http://www.parliament.nz/en-nz/mpp/mps/current?Criteria.ViewAll=1'
-
 require 'capybara/dsl'
 require 'capybara/mechanize'
 
+MP_LIST_URL = 'http://www.parliament.nz/en-nz/mpp/mps/current?Criteria.ViewAll=1'
+
+module Parliament
+  BASE_URL = 'http://www.parliament.nz'
+  class MPScraper
+    def initialize
+      Capybara.current_driver = :mechanize
+      Capybara.run_server = false
+      Capybara.app_host = BASE_URL
+    end
+  end
+end
